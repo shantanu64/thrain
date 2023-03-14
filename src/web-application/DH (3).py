20ea6acbd53479678e48977bc62e319c381411d2
Shantanu Khandelwal <69839664+shantanu64@users.noreply.github.com>
2023-03-14 19:08:59 +0530

    Add files via upload

---
+++ src/web-application/DH (3).py
@@ -0,0 +1,18 @@
+import hashlib
+import ssl
+import binascii
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+def generate_private_key():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+        backend=default_backend()
+    )
+    return private_key
+
+def generate_public_key(private_key):
+	public_key = private_key.public_key()
+	return public_key
